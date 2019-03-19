7de67d1c2050cdebe837f5bc972a65c20b5c2a96
Aleksandr Bukhalo <aleksandr@bukhalo.com>
2019-03-19 22:55:22 +0300

    [HDASH-10] add signup form for test atlaskit

--- src/App.js
+++ src/App.js
@@ -1,57 +1,21 @@
 import React from "react";
-import styled, { keyframes } from "styled-components";
-import logo from "./logo.svg";
+import styled from "styled-components";
 
-const logoSpin = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
+import SignUp from "./components/SignUp";
 
 const Main = styled.div`
-  text-align: center;
-`;
-
-const Header = styled.header`
-  background-color: #282c34;
+  width: 100%;
+  height: 100%;
   min-height: 100vh;
   display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-`;
-
-const Logo = styled.img`
-  animation: ${logoSpin} infinite 20s linear;
-  height: 40vmin;
-  pointer-events: none;
-`;
-
-const Link = styled.a`
-  color: #61dafb;
 `;
 
 const App = () => {
   return (
     <Main>
-      <Header>
-        <Logo src={logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Link
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </Link>
-      </Header>
+      <SignUp />
     </Main>
   );
 };
