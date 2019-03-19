6d80b3ab2da7b2c3328dcc64d59a9a2f32ad724d
Aleksandr Bukhalo <aleksandr@bukhalo.com>
2019-03-19 22:05:20 +0300

    [HDASH-4] install addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,11 @@
-import { configure } from '@storybook/react';
+import {
+  configure
+} from '@storybook/react';
+import '@storybook/addon-console';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
+
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
