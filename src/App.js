12175c95117d5ff0b4568749d9dbcc0c6eb392bc
Aleksandr Bukhalo <aleksandr@bukhalo.com>
2019-03-19 19:39:52 +0300

    [HDASH-5] rewrite example to styled components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
 import logo from "./logo.svg";
-import "./App.css";
+
+const logoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Main = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const Logo = styled.img`
+  animation: ${logoSpin} infinite 20s linear;
+  height: 40vmin;
+  pointer-events: none;
+`;
+
+const Link = styled.a`
+  color: #61dafb;
+`;
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Main>
+      <Header>
+        <Logo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <Link
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </Link>
+      </Header>
+    </Main>
   );
 };
 
